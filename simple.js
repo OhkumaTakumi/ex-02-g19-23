a735080efc70d2df32bbca231f3accfdfb3c238e
Ueda <to24toro@gmail.com>
2019-05-25 20:32:43 +0900

    PUT 実装

--- simple.js
+++ simple.js
@@ -137,6 +137,47 @@ server.on('request', function(req, res) {
                 res.end('Bad Request');
             }
             break;
+            case 'PUT':
+            if(path.match(/^\/id\/\d+$/)) {
+            // $ curl -i -v --data '{"data":"testdata"}' -X PUT http://127.0.0.1:3000/id/:number
+                const number = Number(path.replace( /\/id\//g , "" )) // :numberを特定
+                var item;
+                var data = '';
+                req.on('data', function(chunk){
+                    data += chunk;
+                });
+                req.on('end', function(){
+                    try {
+                        item = JSON.parse(data, function(key, value) {
+                            if(key === '' ) return value;
+                            if(key === 'data') return value;
+                        });
+                    } catch(e) {
+                        res.statusCode = 400;
+                        res.statusMessage = e.message;
+                        res.end('Bad Request');
+                    }
+                    if('data' in item && items.some(function(v, i){return (v.id==number)})){
+                        items[number].data = item.data
+                        var body = JSON.stringify(item, null, '\t');
+                        res.setHeader('Content-Length', Buffer.byteLength(body));
+                        res.setHeader('Content-Type', 'application/json; charset=utf8');
+                        res.statusCode = 200;
+                        res.statusMessage = 'OK';
+                        res.end(body);
+                    }else{
+                        //指定されたidがない場合の処理
+                        res.statusCode = 404;
+                        res.statusMessage = ('Not Found');
+                        res.end('Not Found');
+                    }
+                });
+            } else {
+                res.statusCode = 400;
+                res.statusMessage = ('Bad Request');
+                res.end('Bad Request');
+            }
+            break;
           case 'DELETE':
               if(path.match(/^\/id\/\d+$/)) {
               // $ curl -i -X DELETE http://127.0.0.1:3000/id/:Num
